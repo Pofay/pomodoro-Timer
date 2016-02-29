055a6e00f0969ea7fdb5f6ecce00a75c9d95c57f
Gian Carlo Gilos <giancarlo.gilos@outlook.com>
2016-02-29 16:00:11 +0800

    We can now start a Task via the TaskExecutor

---
+++ app/src/main/java/com/donbosco/giancarlo/focusedsingularity/TaskExecutor.java
@@ -0,0 +1,16 @@
+package com.donbosco.giancarlo.focusedsingularity;
+
+/**
+ * Created by GianCarlo on 2/29/2016.
+ */
+public class TaskExecutor {
+    private Task task;
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public void start() {
+
+    }
+}
