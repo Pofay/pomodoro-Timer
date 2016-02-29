4edf77c22deeadfb1f6de02cfe6ae402fe49e562
Gian Carlo Gilos <giancarlo.gilos@outlook.com>
2016-02-29 09:26:22 +0800

    Initial Commit

---
+++ app/src/test/java/com/donbosco/giancarlo/focusedsingularity/TimerTest.java
@@ -0,0 +1,14 @@
+package com.donbosco.giancarlo.focusedsingularity;
+
+import org.junit.Test;
+
+/**
+ * Created by GianCarlo on 2/28/2016.
+ */
+public class TimerTest {
+
+    @Test
+    public void testInject(){
+        
+    }
+}
