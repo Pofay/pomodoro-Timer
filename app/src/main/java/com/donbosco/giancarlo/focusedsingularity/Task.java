fc63c227f81100516dc0763964a9a4bd678e6106
Gian Carlo Gilos <giancarlo.gilos@outlook.com>
2016-03-02 21:36:39 +0800

    We can now start a task

---
+++ app/src/main/java/com/donbosco/giancarlo/focusedsingularity/Task.java
@@ -0,0 +1,16 @@
+package com.donbosco.giancarlo.focusedsingularity;
+
+/**
+ * Created by GianCarlo on 3/2/2016.
+ */
+public class Task {
+    public void start() {
+    }
+
+    public boolean isStarted() {
+        return true;
+    }
+
+    public void stop() {
+    }
+}
--- app/src/test/java/com/donbosco/giancarlo/focusedsingularity/TaskTest.java
+++ app/src/test/java/com/donbosco/giancarlo/focusedsingularity/TaskTest.java
@@ -1,7 +1,35 @@
 package com.donbosco.giancarlo.focusedsingularity;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by GianCarlo on 3/2/2016.
  */
 public class TaskTest {
+
+    @Test
+    public void ItShouldBeAbleToStart(){
+        Task task = new Task();
+
+        task.start();
+
+        assertThat(task.isStarted(), is(true));
+    }
+
+    @Ignore
+    @Test
+    public void ItShouldBeAbleToStop(){
+        Task task = new Task();
+
+        task.start();
+        task.stop();
+
+        assertThat(task.isStarted(), is(false));
+    }
+
+
 }
