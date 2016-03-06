948b43293fa251c0ef8044b655c80f346590a0ec
Gian Carlo Gilos <giancarlo.gilos@outlook.com>
2016-03-06 14:13:02 +0800

    Started to choose a new Solution

---
+++ app/src/test/java/com/donbosco/giancarlo/focusedsingularity/PomodoroTimerTest.java
@@ -0,0 +1,15 @@
+package com.donbosco.giancarlo.focusedsingularity;
+
+import android.os.CountDownTimer;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by GianCarlo on 3/2/2016.
+ */
+public class PomodoroTimerTest {
+
+}
