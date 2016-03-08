8af890c4fd202096f971fb2503f92c4b94b23b5c
Gian Carlo Gilos <giancarlo.gilos@outlook.com>
2016-03-08 13:26:05 +0800

    We can now set the task the timer to work on

--- app/src/main/java/com/donbosco/giancarlo/focusedsingularity/PomodoroTimer.java
+++ app/src/main/java/com/donbosco/giancarlo/focusedsingularity/PomodoroTimer.java
@@ -4,4 +4,11 @@ package com.donbosco.giancarlo.focusedsingularity;
  * Created by GianCarlo on 3/6/2016.
  */
 public class PomodoroTimer {
+
+    public void setTask(Task task) {
+    }
+
+    public Task getSelectedTask() {
+        return new Task();
+    }
 }
--- app/src/test/java/com/donbosco/giancarlo/focusedsingularity/PomodoroTimerTest.java
+++ app/src/test/java/com/donbosco/giancarlo/focusedsingularity/PomodoroTimerTest.java
@@ -5,11 +5,22 @@ import android.os.CountDownTimer;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 /**
  * Created by GianCarlo on 3/2/2016.
  */
 public class PomodoroTimerTest {
+
+    @Test
+    public void ItCanCreateANewTask() {
+        PomodoroTimer timer = new PomodoroTimer();
+
+        timer.setTask(new Task());
+
+        assertThat(timer.getSelectedTask(), is(notNullValue()));
+    }
 
 }
