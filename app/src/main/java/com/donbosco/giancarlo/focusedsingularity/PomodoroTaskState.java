9c15cfe72ab84be54ea07dbd417ca48049378c5a
Gian Carlo Gilos <giancarlo.gilos@outlook.com>
2016-03-10 09:35:06 +0800

    We renamed TimerState to PomodoroTaskState

--- app/src/main/java/com/donbosco/giancarlo/focusedsingularity/PomodoroTaskState.java
+++ app/src/main/java/com/donbosco/giancarlo/focusedsingularity/PomodoroTaskState.java
@@ -3,7 +3,7 @@ package com.donbosco.giancarlo.focusedsingularity;
 /**
  * Created by GianCarlo on 3/9/2016.
  */
-public enum SimplePomodoroTimerState implements TaskState {
+public enum PomodoroTaskState implements TaskState {
     WORKING {
         @Override
         public void execute(Task task) {
--- app/src/test/java/com/donbosco/giancarlo/focusedsingularity/TaskStateTest.java
+++ app/src/test/java/com/donbosco/giancarlo/focusedsingularity/TaskStateTest.java
@@ -1,54 +1,16 @@
 package com.donbosco.giancarlo.focusedsingularity;
 
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import org.junit.Test;
 
 /**
  * Created by GianCarlo on 3/9/2016.
  */
 public class TaskStateTest {
 
-    private TaskState state;
 
-    @Before
-    public void WhenInWorkingState() {
-        state = SimplePomodoroTimerState.WORKING;
-    }
-
-    @Ignore
     @Test
-    public void ItShouldAddTimeSpentToATaskOnExecute() {
-        Task task = new PomodoroTask("Programming", 2) {
+    public void testName() throws Exception {
 
-            @Override
-            protected void sleep(int timeOut) {
-            }
-
-        };
-
-        state.execute(task);
-
-        assertThat(task.getTimeSpent(), is(2000L));
-    }
-
-    @Ignore
-    @Test
-    public void ItShouldAddTimeSpentToATask() throws Exception {
-        Task task = new PomodoroTask("Stuff", 1) {
-
-            @Override
-            protected void sleep(int timeOut) {
-            }
-
-        };
-
-
-        state.execute(task);
-
-        assertThat(task.getTimeSpent(), is(1000L));
     }
 }
