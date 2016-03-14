9da3f3402ad6bf417f6e1d9a54348adb9e5dc555
Gian Carlo Gilos <giancarlo.gilos@outlook.com>
2016-03-14 16:27:21 +0800

    We added basic setup context in TimerTest

--- app/src/test/java/com/donbosco/giancarlo/focusedsingularity/PomodoroTimerTest.java
+++ app/src/test/java/com/donbosco/giancarlo/focusedsingularity/PomodoroTimerTest.java
@@ -1,6 +1,9 @@
 package com.donbosco.giancarlo.focusedsingularity;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import de.bechte.junit.runners.context.HierarchicalContextRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -9,37 +12,42 @@ import static org.junit.Assert.assertThat;
 /**
  * Created by GianCarlo on 3/2/2016.
  */
+@RunWith(HierarchicalContextRunner.class)
 public class PomodoroTimerTest {
 
-    @Test
-    public void ItCanSetItsTaskToWorkOn() {
-        PomodoroTimer timer = new PomodoroTimer();
 
-        int timeEstimateInSeconds = 25000;
-        timer.setTask(new PomodoroTask("Programming", timeEstimateInSeconds));
+    public class BasicSetUpContext {
+        @Test
+        public void ItCanSetItsTaskToWorkOn() {
+            PomodoroTimer timer = new PomodoroTimer();
 
-        assertThat(timer.getSelectedTask(), is(notNullValue()));
+            int timeEstimateInSeconds = 25000;
+            timer.setTask(new PomodoroTask("Programming", timeEstimateInSeconds));
+
+            assertThat(timer.getSelectedTask(), is(notNullValue()));
+        }
+
+        @Test
+        public void ItShouldHaveTheRightDetailsInTask() {
+            PomodoroTimer timer = new PomodoroTimer();
+
+            int timeEstimateInSeconds = 30000;
+            timer.setTask(new PomodoroTask("Studying", timeEstimateInSeconds));
+
+            PomodoroTask actualTask = timer.getSelectedTask();
+            assertThat(actualTask.getName(), is("Studying"));
+            assertThat(actualTask.getTimeEstimateInSeconds(), is(30000L));
+        }
+
+        @Test
+        public void ItCanHaveItsPomodoroDurationSet() {
+            PomodoroTimer timer = new PomodoroTimer();
+
+            timer.setPomodoroDuration(20L);
+
+            assertThat(timer.getPomodoroDuration(), is(20L));
+        }
     }
 
-    @Test
-    public void ItShouldHaveTheRightDetailsInTask() {
-        PomodoroTimer timer = new PomodoroTimer();
-
-        int timeEstimateInSeconds = 30000;
-        timer.setTask(new PomodoroTask("Studying", timeEstimateInSeconds));
-
-        PomodoroTask actualTask = timer.getSelectedTask();
-        assertThat(actualTask.getName(), is("Studying"));
-        assertThat(actualTask.getTimeEstimateInSeconds(), is(30000L));
-    }
-
-    @Test
-    public void ItCanHaveItsPomodoroDurationSet() {
-        PomodoroTimer timer = new PomodoroTimer();
-
-        timer.setPomodoroDuration(20L);
-
-        assertThat(timer.getPomodoroDuration(), is(20L));
-    }
 
 }
